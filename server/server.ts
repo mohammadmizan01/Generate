306d07860bf1c941146a6d5f5795ab88e1dcb612
mohammadmizna01 <mohammadmizan14257@gmail.com>
2026-05-07 17:17:51 +0530

    establish authentication using better_auth_ui

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 
 // Middleware
-app.use(express.json());
+
 
 const corsOptions = {
     origin: process.env.TRUSTED_ORIGINS?.split(',') || [],
@@ -13,7 +15,9 @@ const corsOptions = {
 }
 app.use(cors(corsOptions ));
 
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
